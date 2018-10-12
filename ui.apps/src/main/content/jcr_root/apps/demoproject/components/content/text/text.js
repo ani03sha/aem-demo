ce44e838010f33527228dcc713e01af2887d20ef
Anirudh Sharma <anirudh03sharma@gmail.com>
2018-10-12 11:15:43 +0530

    Committing the text component illustrating the JS Use API

---
+++ ui.apps/src/main/content/jcr_root/apps/demoproject/components/content/text/text.js
@@ -0,0 +1,10 @@
+"use strict";
+use(function() {
+
+    var text = {};
+
+    text.title = granite.resource.properties["title"];
+    text.description = granite.resource.properties["description"];
+
+    return text;
+});
