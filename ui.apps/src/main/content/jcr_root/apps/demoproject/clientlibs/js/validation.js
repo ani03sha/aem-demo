d1397608470fdf756a607ee298196db229b03846
Anirudh Sharma <anirudh03sharma@gmail.com>
2018-10-15 10:43:51 +0530

    Committing the Client Libraries related code

---
+++ ui.apps/src/main/content/jcr_root/apps/demoproject/clientlibs/js/validation.js
@@ -0,0 +1,13 @@
+$(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
+  selector: "#alt-special", // validates the specific alt field  
+  validate: function(el) {
+    var $el = $(el);
+    var $form = $el.closest('form'); // get the form
+    var $upload = $form.find("coral-fileupload[name$=image]"); // find the file-upload widget
+    if($upload.hasClass('is-filled') && !$el.val()){ // if class exists, return validation msg
+        return "Enter Alt Text";
+    } else {
+        return;
+    }
+  }
+});
