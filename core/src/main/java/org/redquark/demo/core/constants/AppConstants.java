7671cbe49b63038c034eae77d1d0754090fa452b
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-06-05 19:59:01 +0530

    Added sample code to upload a binary file in S3 bucket

--- core/src/main/java/org/redquark/demo/core/constants/AppConstants.java
+++ core/src/main/java/org/redquark/demo/core/constants/AppConstants.java
@@ -14,15 +14,23 @@ public final class AppConstants {
 	public static final String WEATHER_API_ENDPOINT = "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=<YOUR-API_KEY>";
 
 	public static final String SUBSERVICE_NAME = "demoService";
-	
+
 	public static final String SERVICE_USER_ID = "redquark";
-	
+
 	public static final String SERVICE_USER_GROUP_NAME = "administrators";
 
 	public static final String INTERMEDIATE_PATH = "/home/users/system";
 
 	public static final String USER_MAPPER_SERVICE_REFERENCE = "org.apache.sling.serviceusermapping.impl.ServiceUserMapperImpl";
-	
+
 	public static final String USER_MAPPING = "user.mapping";
+
+	public static final String S3_ENDPOINT_URL = "http://redquark-aem-demo.s3-website.ap-south-1.amazonaws.com";
+
+	public static final String S3_ACCESS_KEY = "*****";
+
+	public static final String S3_SECRET_KEY = "*****";
+
+	public static final String S3_BUCKET_NAME = "redquark-aem-demo";
 
 }
